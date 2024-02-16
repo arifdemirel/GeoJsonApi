0a15560bfcae2f79f355a25ca3202d65c12537d6
Arif Demirel <arif_demirel@yahoo.com>
2024-02-16 14:11:13 +0300

    Configured the controller to use FeatureCollection instead of  Feature.

--- GeoJsonApi/Controllers/SpatialDataController.cs
+++ GeoJsonApi/Controllers/SpatialDataController.cs
@@ -23,12 +23,34 @@ namespace GeoJsonApi.Controllers
         public IActionResult Post([FromBody] JsonElement geoJson)
         {
             var geoJsonReader = new GeoJsonReader();
+            var geoJsonText = geoJson.GetRawText();
             //var geometry = geoJsonReader.Read<Geometry>(geoJson.GetRawText());
-            Feature feature = geoJsonReader.Read<Feature>(geoJson.GetRawText());
-            Geometry geometry = feature.Geometry;
+            //Feature feature = geoJsonReader.Read<Feature>(geoJson.GetRawText());
+            FeatureCollection featureCollection = geoJsonReader.Read<FeatureCollection>(geoJsonText);
 
-            var spatialData = new SpatialData { Geometry = geometry };
-            _context.SpatialDatas.Add(spatialData);
+            List<SpatialData> spatialDataList = new List<SpatialData>();
+
+            foreach (var feature in featureCollection) 
+            {
+                Geometry geometry = feature.Geometry;
+
+                SpatialData spatialData = new SpatialData
+                {
+                    Geometry = geometry
+                    // Populate other properties as necessary
+                };
+                spatialDataList.Add(spatialData);
+            }
+
+
+            //var spatialData = new SpatialData { Geometry = geometry };
+            //_context.SpatialDatas.Add(spatialData);
+            //_context.SaveChanges();
+
+            foreach (var spatialData in spatialDataList)
+            {
+                _context.SpatialDatas.Add(spatialData);
+            }
             _context.SaveChanges();
 
             return Ok();
